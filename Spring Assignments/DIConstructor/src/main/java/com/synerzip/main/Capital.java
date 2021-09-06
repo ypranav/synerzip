0279c76d3621487073baeecf751a4ab863721a5f
ypranav <pranavpanhale2@gmail.com>
2021-09-06 19:42:19 +0530

    Add files via upload

---
+++ Spring Assignments/DIConstructor/src/main/java/com/synerzip/main/Capital.java
@@ -0,0 +1,17 @@
+package com.synerzip.main;
+
+public class Capital {
+	 
+    String capitalName;
+ 
+    public String getCapitalName() {
+        return capitalName;
+    }
+
+	public Capital(String capitalName) {
+		super();
+		this.capitalName = capitalName;
+	}
+ 
+}
+ 
